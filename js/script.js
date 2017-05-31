242ced994ace129311037c4816b337eaac0e0d68
Daniella <daniella.aum@gmail.com>
2017-05-31 16:53:59 +0300

    scroll to element on menu click

--- js/script.js
+++ js/script.js
@@ -6,4 +6,20 @@ $(document).ready(function ($) {
         return $(this).ekkoLightbox();
     });
 
+    $(".menu_wrapper ul > li:nth-child(3) > a").on("click", function() {
+
+        $('html, body').animate({
+            scrollTop: $(".image-gallery-wrapper").offset().top
+        }, 1000);
+    });
+
+    $(".menu_wrapper ul > li:nth-child(4) > a").on("click", function() {
+
+        $('html, body').animate({
+            scrollTop: $(".bot1_wrapper").offset().top
+        }, 1000);
+    });
+
+
+
 });
